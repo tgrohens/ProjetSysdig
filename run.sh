f46d4de41ea01f0a00f9f285734a7641ba52255f
Paul Kirchner <paul.kirchne@ens.fr>
2015-01-26 13:02:23 +0100

    Débugué

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
-!/bin/sh
+#!/bin/sh
 cd sim
 sh COMPIL.sh
+cp netlist_sim.native ..
+cp scheduler_test.native ..
 cd ../assembleur
 make
 cd ..
 ./mjc.native proco.mj
 ./assembleur/arm-as clock.s
-./sim/netlist_sim.native proco.net < clock.byte
+./scheduler_test.native proco.net < clock.byte
